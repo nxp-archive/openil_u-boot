9b15b6c306ffa5f809e703548228d74d529d233c
jiaheng.fan <jiaheng.fan@nxp.com>
2017-03-21 17:06:06 +0800

    OPTEE: LS1021AQDS: Platform specific OP-TEE enablement.
    - Added new ls1021aqds_nor_SECURE_BOOT_TEE_defconfig to enable
      CONFIG_ARMV7_TEE which is top level macro to enable op-tee os.
    - CONFIG_TEE_RAM_SIZE defined to allocate memory for op-tee os from top of ram.
      This memory is not be visible to the next level non secure os (Linux in our case).
    - Set up TZASC to secure TEE ram area:
      - Region 0: Default region.
      - Region 1: Secure TEE ram.
      - Region 2: Further divided into 8 sub-regions. Upper 2 sub-regions are having
        both secure & non-secure access(NSEC Shared Memory b/w TEE & REE).
        Rest 6 sub-regions have secure access only(Secure TEE Ram).
    - CONFIG_OPTEE_ENTRY defines the entry point of op-tee os on secure ram.
    - OPTEE_HEADER_START defines starting address of op-tee os image header.
    - OPTEE_IMAGE_START defines the starting address of op-tee os binary.
    - OPTEE_IMAGE_SIZE defines the size of op-tee os binary to be copied
      to secure ram area.
    
    Signed-off-by: jiaheng.fan <jiaheng.fan@nxp.com>
    Signed-off-by: sumit.garg  <sumit.garg@nxp.com>

--- board/freescale/ls1021aqds/ls1021aqds.c
+++ board/freescale/ls1021aqds/ls1021aqds.c
@@ -23,6 +23,7 @@
 #include <fsl_devdis.h>
 #include <fsl_validate.h>
 #include <fsl_ddr.h>
+#include <asm/tzasc380.h>
 #include "../common/sleep.h"
 #include "../common/qixis.h"
 #include "ls1021aqds_qixis.h"
@@ -163,6 +164,28 @@ int dram_init(void)
 	 */
 	select_i2c_ch_pca9547(I2C_MUX_CH_DEFAULT);
 	gd->ram_size = initdram(0);
+
+
+#ifdef	CONFIG_ARMV7_TEE
+#define	CSU_SEC_ACCESS_REG_OFFSET	(0x21c/4)
+#define	TZASC_BYPASS_MUX_DISABLE	0x4
+#define	CCI_TERMINATE_BARRIER_TX	0x8
+	/* Configure CCI control override register to terminate all barrier transactions */
+	out_le32(((u32 *)CONFIG_SYS_CCI400_ADDR), CCI_TERMINATE_BARRIER_TX);
+	/* Configure CSU secure access register to disable TZASC bypass mux */
+	out_be32(((u32 *)((u32 *)CONFIG_SYS_FSL_CSU_ADDR + CSU_SEC_ACCESS_REG_OFFSET)), TZASC_BYPASS_MUX_DISABLE);
+	/* Set security permissions for region 0 */
+	tzasc_set_region(CONFIG_SYS_FSL_TZASC_ADDR, 0, 0, 0, 0, 0, TZASC_REGION_SECURITY_NSRW, 0);
+
+	/* Set region 1 */
+	tzasc_set_region(CONFIG_SYS_FSL_TZASC_ADDR, 1, TZASC_REGION_ENABLED, CONFIG_OPTEE_ENTRY,
+			0, TZASC_REGION_SIZE_64MB, TZASC_REGION_SECURITY_SRW, 0x80);
+	/* Set region 2 */
+	#define TEE_RAM_UPPER_SUBREGION_OFFSET  0x03800000
+	tzasc_set_region(CONFIG_SYS_FSL_TZASC_ADDR, 2, TZASC_REGION_ENABLED, (CONFIG_OPTEE_ENTRY + TEE_RAM_UPPER_SUBREGION_OFFSET),
+			0, TZASC_REGION_SIZE_8MB, TZASC_REGION_SECURITY_SRW, 0xc0);
+#endif
+
 
 	return 0;
 }
