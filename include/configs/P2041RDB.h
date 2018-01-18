b1336202d1f3a4e5c85535511a164c5af301d1bc
yinbo.zhu <yinbo.zhu@nxp.com>
2018-01-18 11:40:38 +0800

    EHCI: Fix endian access issue on EHCI intinalization
    This issue is exposed after commit 9000eddbase0d
    ("drivers/usb/ehci: Use platform-specific accessors"),
    the wrong endian way of EHCI controller programing will cause
    USB function down.
    
    Configs Affected: P2041-40-R2.0, P3041-R2.0, P4080-40-R3.0,
    P5040-21-R2.1, T1024-R1.0, T1040-42-20-22-R1.1,
    T2080-R1.1, T4240-4160-R2.0
    
    Signed-off-by: yinbo.zhu <yinbo.zhu@nxp.com>

--- include/configs/P2041RDB.h
+++ include/configs/P2041RDB.h
@@ -569,6 +569,7 @@ unsigned long get_board_sys_clk(unsigned long dummy);
 
 #if defined(CONFIG_HAS_FSL_DR_USB) || defined(CONFIG_HAS_FSL_MPH_USB)
 #define CONFIG_USB_EHCI_FSL
+#define CONFIG_EHCI_DESC_BIG_ENDIAN
 #define CONFIG_EHCI_HCD_INIT_AFTER_RESET
 #endif
 
