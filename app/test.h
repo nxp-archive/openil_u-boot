4a9ef57b1836c83c28f67e44c6d18eb4555574fa
Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
2018-03-23 11:15:11 +0800

    baremetal:add drivers test case
    Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>

---
+++ app/test.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2018 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef _FSL_LAYERSCAPE_TEST_H
+#define _FSL_LAYERSCAPE_TEST_H
+
+#ifndef __ASSEMBLY__
+
+int test_gpio(void);
+void test_i2c(void);
+void test_irq_init(void);
+
+#endif
+#endif /* _FSL_LAYERSCAPE_TEST_H */
