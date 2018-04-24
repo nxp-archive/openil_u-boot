20ebe879f0e633357d5dbb2641c6cf94f1819ad6
Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
2018-04-24 18:28:39 +0800

    i2c: assign I2C bus on slave cores
    1. Change i2c driver to assign I2C bus on slave cores.
    2. Add I2c configs on ls1043ardb board.
    
    Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>

--- include/i2c.h
+++ include/i2c.h
@@ -6,6 +6,8 @@
  * (C) Copyright 2001
  * Gerald Van Baren, Custom IDEAS, vanbaren@cideas.com.
  *
+ * Copyright 2018-2019 NXP
+ *
  * SPDX-License-Identifier:	GPL-2.0+
  *
  * The original I2C interface was
@@ -574,6 +576,13 @@ void i2c_dump_msgs(struct i2c_msg *msg, int nmsgs);
 #define CONFIG_SYS_SPD_BUS_NUM		0
 #endif
 
+#ifdef CONFIG_I2C_BUS_CORE_ID_SET
+#define I2C_MXC_BUS_GET_CORE_ID(bus) \
+		CONFIG_SYS_I2C_MXC_I2C##bus##_COREID
+#else
+#define I2C_MXC_BUS_GET_CORE_ID(bus) 0
+#endif
+
 struct i2c_adapter {
 	void		(*init)(struct i2c_adapter *adap, int speed,
 				int slaveaddr);
@@ -591,6 +600,7 @@ struct i2c_adapter {
 	int		slaveaddr;
 	int		init_done;
 	int		hwadapnr;
+	int		coreid;
 	char		*name;
 };
 
@@ -606,6 +616,7 @@ struct i2c_adapter {
 		.slaveaddr	=	_slaveaddr, \
 		.init_done	=	0, \
 		.hwadapnr	=	_hwadapnr, \
+		.coreid		=	I2C_MXC_BUS_GET_CORE_ID(_hwadapnr), \
 		.name		=	#_name \
 };
 
