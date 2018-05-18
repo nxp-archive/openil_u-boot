3851b0e299882c9e8a16d9ff49d8ccfae7b62d73
Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
2018-05-18 12:08:32 +0800

    qspi: qspi resource assign on slave cores
    Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>

--- drivers/spi/fsl_qspi.c
+++ drivers/spi/fsl_qspi.c
@@ -913,6 +913,10 @@ static int fsl_qspi_probe(struct udevice *bus)
 	struct dm_spi_bus *dm_spi_bus;
 	int i, ret;
 
+	if (get_core_id() != CONFIG_FSL_QSPI_COREID) {
+		printf("QSPI is not assigned on this core\n");
+		return;
+	}
 	dm_spi_bus = bus->uclass_priv;
 
 	dm_spi_bus->max_hz = plat->speed_hz;
