1ab9a1786b76111cea60f5450bd6925c05ac48d9
Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
2018-05-18 14:33:11 +0800

    baremetal: configs: resource allocation configs
    Separate resource allocation configs from original config files.
    Customer can configure it easier.
    
    Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>

--- include/configs/ls1046a_common.h
+++ include/configs/ls1046a_common.h
@@ -49,9 +49,6 @@
 /* Generic Timer Definitions */
 #define COUNTER_FREQUENCY		25000000	/* 25MHz */
 
-/* Size of malloc() pool */
-#define CONFIG_SYS_MALLOC_LEN		(CONFIG_ENV_SIZE + 1024 * 1024)
-
 /* Serial Port */
 #define CONFIG_CONS_INDEX		2
 #define CONFIG_SYS_NS16550_SERIAL
@@ -127,11 +124,7 @@
 
 /* I2C */
 #define CONFIG_SYS_I2C
-#define	CONFIG_I2C_BUS_CORE_ID_SET
-#define CONFIG_SYS_I2C_MXC_I2C0_COREID  2
-#define CONFIG_SYS_I2C_MXC_I2C1_COREID  2
-#define CONFIG_SYS_I2C_MXC_I2C2_COREID  3
-#define CONFIG_SYS_I2C_MXC_I2C3_COREID  1
+#define CONFIG_SYS_I2C_MXC
 
 /* PCIe */
 #define CONFIG_PCIE1		/* PCIE controller 1 */
