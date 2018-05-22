76e45af5884b4885114bc611d34cd4436efabe28
Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
2018-05-22 15:34:06 +0800

    baremetal: qspi: fix error when not define COREID
    LS1021aiot board do not need to define CONFIG_FSL_QSPI_COREID,
    fix the error when not define it.
    
    Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>

--- drivers/spi/fsl_qspi.c
+++ drivers/spi/fsl_qspi.c
@@ -913,10 +913,12 @@ static int fsl_qspi_probe(struct udevice *bus)
 	struct dm_spi_bus *dm_spi_bus;
 	int i, ret;
 
+#ifdef CONFIG_FSL_QSPI_COREID
 	if (get_core_id() != CONFIG_FSL_QSPI_COREID) {
 		printf("QSPI is not assigned on this core\n");
 		return;
 	}
+#endif
 	dm_spi_bus = bus->uclass_priv;
 
 	dm_spi_bus->max_hz = plat->speed_hz;
