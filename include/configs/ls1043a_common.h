00e2101846ed1c8ddb41c0b31be7d49f67341a23
Changming Huang <jerry.huang@nxp.com>
2018-07-05 18:19:21 +0800

    ls1043ardb/pcie: enable PCIe baremetal support
    Enable PCIe baremetal support.
    
    Signed-off-by: Changming Huang <jerry.huang@nxp.com>

--- include/configs/ls1043a_common.h
+++ include/configs/ls1043a_common.h
@@ -158,7 +158,9 @@
 #define CONFIG_PCIE1		/* PCIE controller 1 */
 #define CONFIG_PCIE2		/* PCIE controller 2 */
 #define CONFIG_PCIE3		/* PCIE controller 3 */
-
+#ifndef CONFIG_PCI
+#define CONFIG_PCI
+#endif
 #ifdef CONFIG_PCI
 #define CONFIG_NET_MULTI
 #define CONFIG_PCI_SCAN_SHOW
