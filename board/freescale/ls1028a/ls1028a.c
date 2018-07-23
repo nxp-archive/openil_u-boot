b2e8dced632fbc5d667096b9fd63ef01a4e78562
Sudhanshu Gupta <sudhanshu.gupta@nxp.com>
2018-07-23 21:40:39 +0530

    armv8: ls1028a: add checkboard function for LS1028A RDB and QDS board
    Signed-off-by: Rajesh Bhagat <rajesh.bhagat@nxp.com>
    Signed-off-by: Sudhanshu Gupta <sudhanshu.gupta@nxp.com>

--- board/freescale/ls1028a/ls1028a.c
+++ board/freescale/ls1028a/ls1028a.c
@@ -22,6 +22,8 @@
 #include <fsl_immap.h>
 #include <netdev.h>
 
+#include "../common/qixis.h"
+
 DECLARE_GLOBAL_DATA_PTR;
 
 int board_init(void)
@@ -87,4 +89,110 @@ int ft_board_setup(void *blob, bd_t *bd)
 	return 0;
 }
 #endif
+
+
+#ifdef CONFIG_TARGET_LS1028AQDS
+int checkboard(void)
+{
+#ifdef CONFIG_FSL_QIXIS
+	char buf[64];
+#ifndef CONFIG_SD_BOOT
+	u8 sw;
+#endif
+#endif
+
+	puts("Board: LS1028AQDS, boot from ");
+
+#ifdef CONFIG_FSL_QIXIS
+#ifdef CONFIG_SD_BOOT
+	puts("SD\n");
+#else
+
+	sw = QIXIS_READ(brdcfg[0]);
+
+	switch (sw) {
+	case 0x00:
+		printf("Serial NOR flash\n");
+		break;
+	case 0x20:
+		printf("Serial NAND flash\n");
+		break;
+	case 0x60:
+		printf("QSPI Emulator\n");
+		break;
+	default:
+		printf("invalid setting of SW: 0x%x\n", sw);
+		break;
+	}
+#endif
+
+	printf("Sys ID: 0x%02x, Sys Ver: 0x%02x\n",
+	       QIXIS_READ(id), QIXIS_READ(arch));
+
+	printf("FPGA:  v%d (%s), build %d\n",
+	       (int)QIXIS_READ(scver), qixis_read_tag(buf),
+	       (int)qixis_read_minor());
+#endif
+
+	return 0;
+}
+#endif
+
+
+
+#ifdef CONFIG_TARGET_LS1028ARDB
+int checkboard(void)
+{
+	static const char *freq[2] = {"100.00", "Reserved"};
+
+	char buf[64];
+	u8 sw;
+	int clock;
+
+	printf("Board: LS1028A-RDB, ");
+
+#ifdef CONFIG_FSL_QIXIS
+	sw = QIXIS_READ(arch);
+	printf("Board Arch: V%d, ", sw >> 4);
+	printf("Board version: %c, boot from ", (sw & 0xf) + 'A');
+
+	memset((u8 *)buf, 0x00, ARRAY_SIZE(buf));
+
+	sw = QIXIS_READ(brdcfg[0]);
+	sw = (sw & QIXIS_LBMAP_MASK) >> QIXIS_LBMAP_SHIFT;
+
+#ifdef CONFIG_SD_BOOT
+	puts("SD card\n");
+#else
+	switch (sw) {
+	case 0:
+	case 4:
+		printf("NOR\n");
+		break;
+	case 1:
+		printf("NAND\n");
+		break;
+	case 2:
+	case 3:
+		printf("EMU\n");
+		break;
+	default:
+		printf("invalid setting of SW%u\n", QIXIS_LBMAP_SWITCH);
+		break;
+	}
+#endif
+
+	printf("FPGA: v%d.%d\n", QIXIS_READ(scver), QIXIS_READ(tagdata));
+
+	puts("SERDES1 Reference : ");
+	sw = QIXIS_READ(brdcfg[2]);
+	clock = (sw >> 6) & 3;
+	printf("Clock1 = %sMHz ", freq[clock]);
+	clock = (sw >> 4) & 3;
+	printf("Clock2 = %sMHz\n", freq[clock]);
+#endif
+	return 0;
+}
+#endif
+
 
