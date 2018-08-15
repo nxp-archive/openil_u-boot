37cd9ce8e38a627edd82d4bb5df5a98b24ed7e58
Jianchao Wang <jianchao.wang@nxp.com>
2018-08-15 11:11:31 +0800

    baremetal add flexcan and flextimer drivers
    Signed-off-by: Jianchao Wang <jianchao.wang@nxp.com>

--- common/board_r.c
+++ common/board_r.c
@@ -57,6 +57,8 @@
 #include <linux/err.h>
 #include <efi_loader.h>
 #include <asm/interrupt-gic.h>
+#include <flexcan.h>
+#include <flextimer.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -1037,6 +1039,10 @@ init_fnc_t init_sequence_r_slave[] = {
 #ifdef CONFIG_SLAVE_FMAN_CORE
 	initr_net,
 #endif
+#endif
+#if CONFIG_FS_FLEXCAN
+	flexcan_init,
+	flextimer_init,
 #endif
 	run_main_loop,
 };
