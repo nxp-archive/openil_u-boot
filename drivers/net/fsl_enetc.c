45c0ec166167a15d513972482a1803a00030da80
Xiaolin He <xiaolin.he@nxp.com>
2018-12-19 14:19:20 +0800

    dm: pci: ENETC RCIE and Ethernet drivers
    Add PCIe RCIE driver for the ENETC Ethernet controller present on
    NXP Layerscale SoCs. The current ENETC HW supports only ECAM as
    configuration space.
    
    The current support includes basic packet transmit/receive over
    the first two 1G Ethernet ports represented by PCIe Physical
    Functions PF0 and PF1. Each PF can be associated with multiple
    end stations: one port station interface (PSI) and multiple
    virtual station interfaces (VSI). The current support only enables
    the PSI or SI0 of each PF.
    
    Signed-off-by: Catalin Horghidan <catalin.horghidan@nxp.com>
    Signed-off-by: Xiaolin He <xiaolin.he@nxp.com>

---
+++ drivers/net/fsl_enetc.c
@@ -0,0 +1,600 @@
+/*
+ * ENETC ethernet controller driver
+ *
+ * Copyright 2017-2019 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <dm.h>
+#include <errno.h>
+#include <memalign.h>
+#include <asm/io.h>
+#include <pci.h>
+#include <net.h>
+#include <asm/processor.h>
+#include <config.h>
+#include "fsl_enetc.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+static struct pci_device_id enetc_ids[] = {
+	{ PCI_DEVICE(PCI_VENDOR_ID_FREESCALE, 0xe100) },
+	{}
+};
+
+static int pcie_enetc_ofdata_to_platdata(struct udevice *dev)
+{
+	struct enetc_rcie *pcie = dev_get_priv(dev);
+	struct fdt_resource resr;
+	int err;
+
+	err = fdt_get_resource(gd->fdt_blob, dev_of_offset(dev), "reg", 0,
+			       &resr);
+	if (err < 0) {
+		debug("\"reg\" resource not found\n");
+		return err;
+	}
+
+	pcie->cfg_base = map_physmem(resr.start, fdt_resource_size(&resr),
+				     MAP_NOCACHE);
+
+	pcie->busno = dev->seq;
+	return 0;
+}
+
+static inline int pcie_enetc_ecam_config_address(struct enetc_rcie *pcie,
+						 pci_dev_t bdf,
+						 uint offset, void **paddress)
+{
+	unsigned int bus = PCI_BUS(bdf);
+	unsigned int dev = PCI_DEV(bdf);
+	unsigned int fn = PCI_FUNC(bdf);
+	void *addr;
+
+	addr = pcie->cfg_base;
+	addr += ENETC_PF_HDR_ADD(bus - pcie->busno, dev, fn);
+	addr += offset;
+	*paddress = addr;
+
+	return 0;
+}
+
+static int pcie_enetc_ecam_read_config(struct udevice *bus, pci_dev_t bdf,
+				       uint offset, ulong *valuep,
+				       enum pci_size_t size)
+{
+	struct enetc_rcie *pcie = dev_get_priv(bus);
+	void *address;
+	int err;
+
+	err = pcie_enetc_ecam_config_address(pcie, bdf, offset, &address);
+	if (err < 0) {
+		*valuep = pci_get_ff(size);
+		return 0;
+	}
+
+	switch (size) {
+	case PCI_SIZE_8:
+		*valuep = readb(address);
+		return 0;
+	case PCI_SIZE_16:
+		*valuep = readw(address);
+		return 0;
+	case PCI_SIZE_32:
+		*valuep = readl(address);
+		return 0;
+	default:
+		return -EINVAL;
+	}
+}
+
+static int pcie_enetc_ecam_write_config(struct udevice *bus, pci_dev_t bdf,
+					uint offset, ulong value,
+					enum pci_size_t size)
+{
+	struct enetc_rcie *pcie = dev_get_priv(bus);
+	void *address;
+	int err;
+
+	err = pcie_enetc_ecam_config_address(pcie, bdf, offset, &address);
+	if (err < 0)
+		return 0;
+
+	switch (size) {
+	case PCI_SIZE_8:
+		writeb(value, address);
+		return 0;
+	case PCI_SIZE_16:
+		writew(value, address);
+		return 0;
+	case PCI_SIZE_32:
+		writel(value, address);
+		return 0;
+	default:
+		return -EINVAL;
+	}
+}
+
+static int num_devices;
+static int enetc_bind(struct udevice *dev)
+{
+	char name[16];
+
+	sprintf(name, "enetc#%u", num_devices++);
+	return device_set_name(dev, name);
+}
+
+int enetc_init_pf_regs(struct udevice *dev)
+{
+	struct enetc_devfn *hw = dev_get_priv(dev);
+	u64 regs_addr64 = 0ULL;
+	u32 val = 0;
+
+	/* Get PF BARs from EA (enhanced allocation) structures;
+	 * ENETC uses EA and standard PCI header BARs are hardwired to 0
+	 */
+	dm_pci_read_config32(dev, PCI_CFC_EA_CAP_BASE, &val);
+
+	/* check if EA capability exists */
+	if ((val & 0xff) != PCI_CFC_EA_CAP_ID) {
+		ENETC_DBG(hw, "PCIe EA not supported\n");
+		return -EEXIST;
+	}
+
+	/* check number of BARs */
+	if (((val >> 16) & 0x3f) != ENETC_PF_HDR_T0_NUM_BAR) {
+		ENETC_DBG(hw, "No PF regs BARs in EA struct\n");
+		return -EINVAL;
+	}
+
+	val = 0;
+	dm_pci_read_config32(dev, PCI_EA_PF_REG_FORMAT, &val);
+
+	/* check enable and BEI (BAR equiv. indicator) */
+	if (!PCI_EA_PF_REG_FORMAT_ENABLE(val) ||
+	    PCI_EA_PF_REG_FORMAT_BEI(val) != PCI_EA_PF_BEI_BAR0)
+		return -EINVAL;
+
+	hw->regs_base = 0;
+	hw->port_regs = 0;
+	/* read low address */
+	val = 0;
+	dm_pci_read_config32(dev, PCI_EA_PF_BAR_LOW, &val);
+	regs_addr64 = PCI_EA_PF_BASE_REG(val);
+
+	/* read high address if necessary */
+	if (PCI_EA_PF_BASE_REG_IS_64BIT(val)) {
+		val = 0;
+		dm_pci_read_config32(dev, PCI_EA_PF_BAR_HIGH, &val);
+		regs_addr64 |= ((u64)val) << 32;
+	}
+
+	/* read reg. region size */
+	val = 0;
+	dm_pci_read_config32(dev, PCI_EA_PF_BAR_SIZE, &val);
+
+	hw->regs_size = val;
+	/* map BAR address to virtual address; also must have a MMU entry */
+	hw->regs_base = map_physmem(regs_addr64, hw->regs_size, MAP_NOCACHE);
+	hw->port_regs = map_physmem(regs_addr64 + ENETC_PORT_REGS_OFF, 0,
+				    MAP_NOCACHE);
+
+	ENETC_DBG(hw, "regs:0x%x%08x, size:0x%x\n", upper_32_bits(regs_addr64),
+		  lower_32_bits(regs_addr64), hw->regs_size);
+
+	return 0;
+}
+
+/* Initiate PCIe PF ENETC function level reset */
+static int enetc_pf_flr(struct udevice *dev)
+{
+	struct enetc_devfn *hw = dev_get_priv(dev);
+	u32 val = 0;
+
+	dm_pci_read_config32(dev, PCI_CFC_PCIE_DEV_CAP, &val);
+	/* check if FLR capability exists */
+	if (!(val & PCI_CFC_PCIE_FLR_CAP_ID)) {
+		ENETC_ERR(hw, "FLR not supported\n");
+		return -EEXIST;
+	}
+	/* initiate FLR request */
+	dm_pci_write_config16(dev, PCI_CFC_PCIE_DEV_CTL,
+			      PCI_CFC_PCIE_DEV_CTL_INIT_FLR);
+	/* wait ~100ms for FLR completion */
+	udelay(ENETC_DEV_FLR_WAIT_MS);
+
+	/* check if FLR completed */
+	dm_pci_read_config16(dev, PCI_CFC_PCIE_DEV_STAT, (u16 *)&val);
+
+	return val & PCI_CFC_PCIE_DEV_STAT_TRANS_PEND ? -EINVAL : 0;
+}
+
+int enetc_enable_si_port(struct enetc_devfn *hw)
+{
+	u32 val;
+
+	/* set Rx/Tx BDR count */
+	val = ENETC_PSICFGR_SET_TXBDR(ENETC_TX_BDR_CNT);
+	val |= ENETC_PSICFGR_SET_RXBDR(ENETC_RX_BDR_CNT);
+	enetc_write_port(hw, ENETC_PSICFGR(0), val);
+	/* set Rx max frame size */
+	enetc_write_port(hw, ENETC_PM_MAXFRM, ENETC_RX_MAXFRM_SIZE);
+	/* enable MAC port */
+	enetc_write_port(hw, ENETC_PM_CC, ENETC_PM_CC_RX_TX_EN);
+	/* enable port */
+	enetc_write_port(hw, ENETC_PMR, ENETC_PMR_SI0_EN);
+	/* set SI cache policy */
+	enetc_write(hw, ENETC_SICAR0, ENETC_SICAR_RD_CFG | ENETC_SICAR_WR_CFG);
+	/* enable SI */
+	enetc_write(hw, ENETC_SIMR, ENETC_SIMR_EN);
+
+	return 0;
+}
+
+static int enetc_disable_si_port(struct enetc_devfn *hw)
+{
+	/* disable MAC port */
+	enetc_write_port(hw, ENETC_PM_CC, ENETC_PM_CC_DEFAULT);
+
+	/* disable station interface */
+	enetc_write(hw, ENETC_SIMR, 0);
+
+	/* disable station interface ports */
+	enetc_write_port(hw, ENETC_PMR, 0);
+
+	return 0;
+}
+
+/*
+ * Probe ENETC driver:
+ * - initialize port and station interface BARs
+ */
+static int enetc_probe(struct udevice *dev)
+{
+	struct enetc_devfn *hw = dev_get_priv(dev);
+	int ret = 0;
+
+	hw->name = dev->name;
+	hw->devno = trailing_strtol(dev->name);
+	ENETC_DBG(hw, "probing driver ...\n");
+
+	/* initialize register */
+	ret = enetc_init_pf_regs(dev);
+	if (ret < 0) {
+		ENETC_DBG(hw, "failed to initialize PSI reg base\n");
+		return ret;
+	}
+
+	return ret;
+}
+
+/**
+ * Remove ENETC driver:
+ * - disable port and station interface
+ * - run function level reset
+ */
+static int enetc_remove(struct udevice *dev)
+{
+	struct enetc_devfn *hw = dev_get_priv(dev);
+
+	ENETC_DBG(hw, "removing driver ...\n");
+	enetc_disable_si_port(hw);
+
+	/* reset device */
+	enetc_pf_flr(dev);
+
+	return 0;
+}
+
+/* ENETC Port MAC address registers accept big-endian format */
+static void enetc_set_primary_mac_addr(struct enetc_devfn *hw, const u8 *addr)
+{
+	u16 lower = *(const u16 *)(addr + 4);
+	u32 upper = *(const u32 *)addr;
+
+	enetc_write_port(hw, ENETC_PSIPMAR0(hw->devno), upper);
+	enetc_write_port(hw, ENETC_PSIPMAR1(hw->devno), lower);
+}
+
+/* Share rx/tx descriptor buffers since U-Boot does not support multiple
+ * active Ethernet connections.
+ */
+DEFINE_ALIGN_BUFFER(struct enetc_tx_bd, enetc_txbd, ENETC_BD_CNT, ENETC_ALIGN);
+DEFINE_ALIGN_BUFFER(union enetc_rx_bd, enetc_rxbd, ENETC_BD_CNT, ENETC_ALIGN);
+DEFINE_ALIGN_BUFFER(u8, enetc_rx_buff, ENETC_RX_MBUFF_SIZE, ENETC_ALIGN);
+
+inline u64 enetc_rxb_address(struct udevice *dev, int i)
+{
+	int off = i * ENETC_RX_MAXFRM_SIZE;
+
+	return cpu_to_le64(dm_pci_virt_to_mem(dev, enetc_rx_buff + off));
+}
+
+/**
+ * Setup a single Tx BD Ring (ID = 0):
+ * - set Tx buffer descriptor address
+ * - set the BD count
+ * - initialize the producer and consumer index
+ */
+static void enetc_setup_tx_bdr(struct enetc_devfn *hw)
+{
+	struct bd_ring *tx_bdr = &hw->tx_bdr;
+	u64 tx_bd_add = (u64)enetc_txbd;
+
+	/* used later to advance to the next Tx BD */
+	tx_bdr->bd_count = ENETC_BD_CNT;
+	tx_bdr->next_prod_idx = 0;
+	tx_bdr->next_cons_idx = 0;
+	tx_bdr->cons_idx = hw->regs_base +
+				ENETC_BDR(TX, ENETC_TX_BDR_ID, ENETC_TBCIR);
+	tx_bdr->prod_idx = hw->regs_base +
+				ENETC_BDR(TX, ENETC_TX_BDR_ID, ENETC_TBPIR);
+
+	/* set Tx BD address */
+	enetc_bdr_write(hw, TX, ENETC_TX_BDR_ID, ENETC_TBBAR0,
+			lower_32_bits(tx_bd_add));
+	enetc_bdr_write(hw, TX, ENETC_TX_BDR_ID, ENETC_TBBAR1,
+			upper_32_bits(tx_bd_add));
+	/* set Tx 8 BD count */
+	enetc_bdr_write(hw, TX, ENETC_TX_BDR_ID, ENETC_TBLENR,
+			tx_bdr->bd_count);
+
+	/* reset both producer/consumer indexes */
+	enetc_write_reg(tx_bdr->cons_idx, tx_bdr->next_cons_idx);
+	enetc_write_reg(tx_bdr->prod_idx, tx_bdr->next_prod_idx);
+
+	/* enable TX ring */
+	enetc_bdr_write(hw, TX, ENETC_TX_BDR_ID, ENETC_TBMR, ENETC_TBMR_EN);
+}
+
+/**
+ * Setup a single Rx BD Ring (ID = 0):
+ * - set Rx buffer descriptors address (one descriptor per buffer)
+ * - set buffer size as max frame size
+ * - enable Rx ring
+ * - reset consumer and producer indexes
+ * - set buffer for each descriptor
+ */
+static void enetc_setup_rx_bdr(struct udevice *dev, struct enetc_devfn *hw)
+{
+	struct bd_ring *rx_bdr = &hw->rx_bdr;
+	u64 rx_bd_add = (u64)enetc_rxbd;
+	int i;
+
+	/* used later to advance to the next BD produced by ENETC HW */
+	rx_bdr->bd_count = ENETC_BD_CNT;
+	rx_bdr->next_prod_idx = 0;
+	rx_bdr->next_cons_idx = ENETC_RBCI_INIT;
+	rx_bdr->cons_idx = hw->regs_base +
+				ENETC_BDR(RX, ENETC_RX_BDR_ID, ENETC_RBCIR);
+	rx_bdr->prod_idx = hw->regs_base +
+				ENETC_BDR(RX, ENETC_RX_BDR_ID, ENETC_RBPIR);
+
+	/* set Rx BD address */
+	enetc_bdr_write(hw, RX, ENETC_RX_BDR_ID, ENETC_RBBAR0,
+			lower_32_bits(rx_bd_add));
+	enetc_bdr_write(hw, RX, ENETC_RX_BDR_ID, ENETC_RBBAR1,
+			upper_32_bits(rx_bd_add));
+	/* set Rx BD count (multiple of 8) */
+	enetc_bdr_write(hw, RX, ENETC_RX_BDR_ID, ENETC_RBLENR,
+			rx_bdr->bd_count);
+	/* set Rx buffer  size */
+	enetc_bdr_write(hw, RX, ENETC_RX_BDR_ID, ENETC_RBBSR, ENETC_BUFF_SIZE);
+
+	/* reset producer (ENETC owned) and consumer (SW owned) index */
+	enetc_write_reg(rx_bdr->cons_idx, rx_bdr->next_cons_idx);
+	enetc_write_reg(rx_bdr->prod_idx, rx_bdr->next_prod_idx);
+
+	/* fill Rx BD */
+	memset(enetc_rxbd, 0, rx_bdr->bd_count * sizeof(union enetc_rx_bd));
+	for (i = 0; i < rx_bdr->bd_count; i++) {
+		enetc_rxbd[i].w.addr = enetc_rxb_address(dev, i);
+		/* each RX buffer must be aligned to 64B */
+		WARN_ON(enetc_rxbd[i].w.addr & (ENETC_ALIGN - 1));
+	}
+	/* enable Rx ring */
+	enetc_bdr_write(hw, RX, ENETC_RX_BDR_ID, ENETC_RBMR, ENETC_RBMR_EN);
+}
+
+/**
+ * Start ENETC port:
+ * - run function lever reset (FLR)
+ * - enable access to port and SI registers
+ * - set mac address
+ * - setup TX/RX buffer descriptors
+ * - enable Tx/Rx rings
+ */
+static int enetc_start(struct udevice *dev)
+{
+	struct eth_pdata *plat = dev_get_platdata(dev);
+	struct enetc_devfn *hw = dev_get_priv(dev);
+	int ret;
+
+	ENETC_DBG(hw, "starting ...\n");
+
+	/* run FLR on current PF */
+	ENETC_DBG(hw, "reseting ...\n");
+	ret = enetc_pf_flr(dev);
+	if (ret < 0)
+		ENETC_ERR(hw, "failed to reset\n");
+
+	/* enable issue memory I/O requests by this PF - required after FLR */
+	dm_pci_write_config16(dev, PCI_CFH_CMD, PCI_CFH_CMD_IO_MEM_EN);
+
+	if (!is_valid_ethaddr(plat->enetaddr)) {
+		ENETC_DBG(hw, "invalid MAC address, generate random ...\n");
+		net_random_ethaddr(plat->enetaddr);
+	}
+	enetc_set_primary_mac_addr(hw, plat->enetaddr);
+
+	ENETC_DBG(hw, "enabling port and rings ...\n");
+	enetc_enable_si_port(hw);
+
+	/* setup Tx/Rx buffer descriptors */
+	enetc_setup_tx_bdr(hw);
+	enetc_setup_rx_bdr(dev, hw);
+
+	return 0;
+}
+
+/**
+ * Stop ENETC port:
+ * - disable Tx/Rx rings
+ * - disable port and SI
+ */
+static void enetc_stop(struct udevice *dev)
+{
+	struct enetc_devfn *hw = dev_get_priv(dev);
+	u16 v = 0;
+
+	ENETC_DBG(hw, "stopping ...\n");
+
+	/* check if this device was started and we have access to it */
+	dm_pci_read_config16(dev, PCI_CFH_CMD, &v);
+	if ((v & PCI_CFH_CMD_IO_MEM_EN) != PCI_CFH_CMD_IO_MEM_EN)
+		return;
+
+	ENETC_DBG(hw, "disabling port and rings ...\n");
+
+	/* disable Tx/Rx rings */
+	enetc_bdr_write(hw, TX, ENETC_TX_BDR_ID, ENETC_TBMR, 0);
+	enetc_bdr_write(hw, RX, ENETC_RX_BDR_ID, ENETC_RBMR, 0);
+
+	/* disable port and SI */
+	enetc_disable_si_port(hw);
+}
+
+/**
+ * ENETC transmit packet:
+ * - check if Tx BD ring is full
+ * - set buffer/packet address (dma address)
+ * - set final fragment flag
+ * - try while producer index equals consumer index or timeout
+ */
+static int enetc_send(struct udevice *dev, void *packet, int length)
+{
+	struct enetc_devfn *hw = dev_get_priv(dev);
+	struct bd_ring *txr = &hw->tx_bdr;
+	void *nv_packet = (void *)packet;
+	int tries = ENETC_POLL_TRIES;
+	u32 pi, ci;
+
+	pi = txr->next_prod_idx;
+	ci = enetc_read_reg(txr->cons_idx) & ENETC_BDR_IDX_MASK;
+	/* Tx ring is full when */
+	if (((pi + 1) % txr->bd_count) == ci) {
+		ENETC_DBG(hw, "Tx BDR full\n");
+		return -ETIMEDOUT;
+	}
+	ENETC_DBG(hw, "TxBD[%d]send: pkt_len=%d, buff @0x%x%08x\n", pi, length,
+		  upper_32_bits((u64)nv_packet), lower_32_bits((u64)nv_packet));
+
+	/* prepare Tx BD */
+	memset(&enetc_txbd[pi], 0x0, sizeof(struct enetc_tx_bd));
+	enetc_txbd[pi].addr =  cpu_to_le64(dm_pci_virt_to_mem(dev, nv_packet));
+	enetc_txbd[pi].buf_len = cpu_to_le16(length);
+	enetc_txbd[pi].frm_len = cpu_to_le16(length);
+	enetc_txbd[pi].flags = cpu_to_le16(ENETC_TXBD_FLAGS_F);
+	dmb();
+	/* send frame: increment producer index */
+	pi = (pi + 1) % txr->bd_count;
+	txr->next_prod_idx = pi;
+	enetc_write_reg(txr->prod_idx, pi);
+	while ((--tries >= 0) &&
+	       (pi != (enetc_read_reg(txr->cons_idx) & ENETC_BDR_IDX_MASK)))
+		cpu_relax();
+
+	return tries > 0 ? 0 : -ETIMEDOUT;
+}
+
+/**
+ * Handles frame receive and cleans up the BD slot on the Rx ring.
+ */
+static int enetc_recv(struct udevice *dev, int flags, uchar **packetp)
+{
+	struct enetc_devfn *hw = dev_get_priv(dev);
+	struct bd_ring *rxr = &hw->rx_bdr;
+	int tries = ENETC_POLL_TRIES;
+	int pi = rxr->next_prod_idx;
+	int ci = rxr->next_cons_idx;
+	u32 status;
+	int len;
+	u8 rdy;
+
+	do {
+		dmb();
+		status = le32_to_cpu(enetc_rxbd[pi].r.lstatus);
+		/* check if current BD is ready to be consumed */
+		rdy = ENETC_RXBD_STATUS_R(status);
+	} while (--tries >= 0 && !rdy);
+
+	if (!rdy)
+		return -EAGAIN;
+
+	dmb();
+	len = le16_to_cpu(enetc_rxbd[pi].r.buf_len);
+	*packetp = (uchar *)enetc_rxb_address(dev, pi);
+	ENETC_DBG(hw, "RxBD[%d]: len=%d err=%d pkt=0x%x%08x\n", pi, len,
+		  ENETC_RXBD_STATUS_ERRORS(status),
+		  upper_32_bits((u64)*packetp), lower_32_bits((u64)*packetp));
+
+	/* BD clean up and advance to next in ring */
+	memset(&enetc_rxbd[pi], 0, sizeof(union enetc_rx_bd));
+	enetc_rxbd[pi].w.addr = enetc_rxb_address(dev, pi);
+	rxr->next_prod_idx = (pi + 1) % rxr->bd_count;
+	ci = (ci + 1) % rxr->bd_count;
+	rxr->next_cons_idx = ci;
+	dmb();
+	/* let ENETC HW know we free up the slot in the ring */
+	enetc_write_reg(rxr->cons_idx, ci);
+
+	return len;
+}
+
+static const struct dm_pci_ops pcie_enetc_ops = {
+	.read_config	= pcie_enetc_ecam_read_config,
+	.write_config	= pcie_enetc_ecam_write_config,
+};
+
+static const struct eth_ops enetc_ops = {
+	.start	= enetc_start,
+	.send	= enetc_send,
+	.recv	= enetc_recv,
+	.stop	= enetc_stop,
+};
+
+static const struct udevice_id ls_enetc_rcie_ids[] = {
+	{ .compatible = "fsl,ls-enetc-rcie" },
+	{ }
+};
+
+static const struct udevice_id ls_enetc_eth_ids[] = {
+	{ .compatible = "fsl,ls-enetc-eth" },
+	{ }
+};
+
+U_BOOT_DRIVER(pci_enetc) = {
+	.name	= "ls_enetc_rcie",
+	.id		= UCLASS_PCI,
+	.of_match	= ls_enetc_rcie_ids,
+	.ops	= &pcie_enetc_ops,
+	.ofdata_to_platdata	= pcie_enetc_ofdata_to_platdata,
+	.priv_auto_alloc_size	= sizeof(struct enetc_rcie),
+};
+
+U_BOOT_DRIVER(eth_enetc) = {
+	.name	= "ls_enetc_eth",
+	.id		= UCLASS_ETH,
+	.of_match	= ls_enetc_eth_ids,
+	.bind	= enetc_bind,
+	.probe	= enetc_probe,
+	.remove = enetc_remove,
+	.ops	= &enetc_ops,
+	.priv_auto_alloc_size = sizeof(struct enetc_devfn),
+	.platdata_auto_alloc_size = sizeof(struct eth_pdata),
+};
+
+U_BOOT_PCI_DEVICE(pci_enetc, enetc_ids);
+U_BOOT_PCI_DEVICE(eth_enetc, enetc_ids);
---
+++ drivers/net/fsl_enetc.h
@@ -0,0 +1,223 @@
+/*
+ * ENETC ethernet controller driver
+ *
+ * Copyright 2017-2019 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#ifdef DEBUG
+#define ENETC_DEBUG
+#endif
+
+#define ENETC_ERR(hw, fmt, args...) \
+	printf("enetc[%u]: ERROR: " fmt, (hw)->devno, ##args)
+
+#ifdef ENETC_DEBUG
+#define ENETC_DBG(hw, fmt, args...) \
+	printf("enetc[%u]: DEBUG: " fmt, (hw)->devno, ##args)
+#else
+#define ENETC_DBG(devno, args...)	do { } while (0)
+#endif
+
+/* PCI and RCIE */
+#define PCIE_CONFIG_SPACE_SIZE 0x1000
+#define ENETC_PF_HDR_ADD(b, d, f) ((((b) << 8) + ((d) << 3) + (f)) \
+					* PCIE_CONFIG_SPACE_SIZE)
+/* PCI Config header command */
+#define PCI_CFH_CMD 0x4
+/* Enable memory access */
+#define PCI_CFH_CMD_IO_MEM_EN (BIT(1) | BIT(2))
+
+/* PCIe device capabilities */
+#define PCI_CFC_PCIE_DEV_CAP 0x44
+#define PCI_CFC_PCIE_FLR_CAP_ID BIT(28)
+#define PCI_CFC_PCIE_DEV_CTL 0x48
+#define PCI_CFC_PCIE_DEV_CTL_INIT_FLR BIT(15)
+#define PCI_CFC_PCIE_DEV_STAT 0x4a
+#define PCI_CFC_PCIE_DEV_STAT_TRANS_PEND BIT(5)
+
+/* After initiating FLR SW must wait before reading the reset status */
+#define ENETC_DEV_FLR_WAIT_MS 100
+
+/* PCI Enhanced Allocation capabilities */
+#define PCI_CFC_EA_CAP_BASE 0x9c
+/* Hardwired value for EA capability */
+#define PCI_CFC_EA_CAP_ID 0x14
+#define PCI_CFC_EA_CAP 0x9e
+/* number of BARs of PF for ENETC ports */
+#define ENETC_PF_HDR_T0_NUM_BAR 4
+
+/* BAR 0 ID for device regs. access */
+#define PCI_EA_PF_BEI_BAR0 0
+
+/* offsets within PCI EA capability structures only */
+#define PCI_EA_PF_REG_FORMAT (PCI_CFC_EA_CAP_BASE + 0x4)
+#define PCI_EA_PF_BAR_LOW (PCI_CFC_EA_CAP_BASE + 0x8)
+#define PCI_EA_PF_BAR_SIZE (PCI_CFC_EA_CAP_BASE + 0xc)
+#define PCI_EA_PF_BAR_HIGH (PCI_CFC_EA_CAP_BASE + 0x10)
+#define PCI_EA_PF_REG_FORMAT_BEI(x) (((x) >> 4) & 0xf)
+#define PCI_EA_PF_REG_FORMAT_ENABLE(x) (((x) >> 31) & 0x1)
+#define PCI_EA_PF_BASE_REG(x) ((x) & ~0x3)
+#define PCI_EA_PF_SIZE(x) ((x) & ~0x3)
+#define PCI_EA_PF_BASE_REG_IS_64BIT(x) ((x) & 0x2)
+
+/* PCI Root Complex info */
+struct enetc_rcie {
+	const char *name;
+	int busno;
+	void *cfg_base;
+};
+
+/* ENETC controller registers */
+
+/* Station interface register offsets */
+#define ENETC_SIMR	0
+#define ENETC_SIMR_EN	BIT(31)
+#define ENETC_SICAR0	0x40
+/* write cache cfg: snoop, no allocate, update full (data), partial (BD) */
+#define ENETC_SICAR_WR_CFG	0x6767
+/* read cache cfg: coherent copy, look up, no allocate */
+#define ENETC_SICAR_RD_CFG	0x27270000
+
+#define ENETC_SIROCT	0x300
+#define ENETC_SIRFRM	0x308
+#define ENETC_SITOCT	0x320
+#define ENETC_SITFRM	0x328
+
+/* Station Interface Rx/Tx Buffer Descriptor Ring registers */
+enum enetc_bdr_type {TX, RX};
+#define ENETC_BDR(type, n, off)	(0x8000 + (type) * 0x100 + (n) * 0x200 + (off))
+#define ENETC_BDR_IDX_MASK	0xffff
+
+/* Rx BDR reg offsets */
+#define ENETC_RBMR	0
+#define ENETC_RBMR_EN	BIT(31)
+#define ENETC_RBBSR	0x8
+#define ENETC_RBCIR	0xc
+/* initial consumer index for Rx BDR */
+#define ENETC_RBCI_INIT 4
+#define ENETC_RBBAR0	0x10
+#define ENETC_RBBAR1	0x14
+#define ENETC_RBPIR	0x18
+#define ENETC_RBLENR	0x20
+
+/* Tx BDR reg offsets */
+#define ENETC_TBMR	0
+#define ENETC_TBMR_EN	BIT(31)
+#define ENETC_TBBAR0	0x10
+#define ENETC_TBBAR1	0x14
+#define ENETC_TBPIR	0x18
+#define ENETC_TBCIR	0x1c
+#define ENETC_TBLENR	0x20
+
+/* Port registers offset */
+#define ENETC_PORT_REGS_OFF 0x10000
+#define ENETC_PMR	0x00000
+
+#define ENETC_PMR_SI0_EN	BIT(16)
+#define ENETC_PSIPMMR	0x18
+#define ENETC_PSIPMAR0(n)	(0x00100 + (n) * 0x20)
+#define ENETC_PSIPMAR1(n)	(0x00104 + (n) * 0x20)
+#define ENETC_PSICFGR(n)	(0x00940 + (n) * 0x10)
+#define ENETC_PSICFGR_SET_TXBDR(val)	((val) & 0xff)
+#define ENETC_PSICFGR_SET_RXBDR(val)	(((val) & 0xff) << 16)
+#define ENETC_PM_CC	0x8008
+/* Port config: enable MAC Tx/Rx, Tx padding, MAC promisc */
+#define ENETC_PM_CC_DEFAULT 0x810
+#define ENETC_PM_CC_RX_TX_EN 0x8813
+#define ENETC_PM_MAXFRM	0x8014
+#define ENETC_RX_MAXFRM_SIZE	PKTSIZE_ALIGN
+#define ENETC_BUFF_SIZE	PKTSIZE_ALIGN
+/* buffer descriptors count must be multiple of 8 and aligned to 128 bytes */
+#define ENETC_BD_CNT 16
+#define ENETC_ALIGN 128
+#define ENETC_RX_MBUFF_SIZE (ENETC_BD_CNT * ENETC_RX_MAXFRM_SIZE)
+
+/* single pair of Rx/Tx rings */
+#define ENETC_RX_BDR_CNT 1
+#define ENETC_TX_BDR_CNT 1
+#define ENETC_RX_BDR_ID 0
+#define ENETC_TX_BDR_ID 0
+
+/* Tx buffer descriptor */
+struct enetc_tx_bd {
+	__le64 addr;
+	__le16 buf_len;
+	__le16 frm_len;
+	__le16 err_csum;
+	__le16 flags;
+};
+
+#define ENETC_TXBD_FLAGS_F	BIT(15)
+#define ENETC_POLL_TRIES 0x8000
+
+/* Rx buffer descriptor */
+union enetc_rx_bd {
+	/* SW provided BD format */
+	struct {
+		__le64 addr;
+		u8 reserved[8];
+	} w;
+
+	/* ENETC returned BD format */
+	struct {
+		__le16 inet_csum;
+		__le16 parse_summary;
+		__le32 rss_hash;
+		__le16 buf_len;
+		__le16 vlan_opt;
+		union {
+			struct {
+				__le16 flags;
+				__le16 error;
+			};
+			__le32 lstatus;
+		};
+	} r;
+};
+
+#define ENETC_RXBD_STATUS_R(status)	(((status) >> 30) & 0x1)
+#define ENETC_RXBD_STATUS_F(status)	(((status) >> 31) & 0x1)
+#define ENETC_RXBD_STATUS_ERRORS(status)	(((status) >> 16) & 0xff)
+#define ENETC_RXBD_STATUS(flags)	((flags) << 16)
+
+/* Tx/Rx ring info */
+struct bd_ring {
+	void *cons_idx;
+	void *prod_idx;
+	/* next BD index to use */
+	int next_prod_idx;
+	int next_cons_idx;
+	int bd_count;
+};
+
+/* ENETC HW access info */
+struct enetc_devfn {
+	const char *name; /* device name */
+	int devno; /* */
+	void *regs_base; /* base ENETC registers */
+	u32 regs_size;
+	void *port_regs; /* base ENETC port registers */
+
+	/* Rx/Tx buffer descriptor rings info */
+	struct bd_ring tx_bdr;
+	struct bd_ring rx_bdr;
+};
+
+/* register accessors */
+#define enetc_read_reg(x)	readl((x))
+#define enetc_write_reg(x, val)	writel((val), (x))
+#define enetc_read(hw, off)	enetc_read_reg((hw)->regs_base + (off))
+#define enetc_write(hw, off, v)	enetc_write_reg((hw)->regs_base + (off), v)
+
+/* port register accessors */
+#define enetc_read_port(hw, off)	enetc_read_reg((hw)->port_regs + (off))
+#define enetc_write_port(hw, off, v)	\
+				enetc_write_reg((hw)->port_regs + (off), v)
+
+/* BDR register accessors, see ENETC_BDR() */
+#define enetc_bdr_read(hw, t, n, off) \
+				enetc_read(hw, ENETC_BDR(t, n, off))
+#define enetc_bdr_write(hw, t, n, off, val) \
+				enetc_write(hw, ENETC_BDR(t, n, off), val)
+
