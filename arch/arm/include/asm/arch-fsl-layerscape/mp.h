3fb99f313c94e7b3a94c622d32548c1b790ecfd0
Xianyu Wei <xianyu.wei@nxp.com>
2018-12-26 15:48:59 +0800

    Revert "ls1043a: support baremetal start"
    This reverts commit 97d1b849534488c189f29eef75055d701ddf6fde.

--- arch/arm/include/asm/arch-fsl-layerscape/mp.h
+++ arch/arm/include/asm/arch-fsl-layerscape/mp.h
@@ -1,5 +1,6 @@
 /*
  * Copyright 2014-2015, Freescale Semiconductor
+ * Copyright 2018-2019 NXP
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
@@ -39,7 +40,6 @@ extern u64 *secondary_boot_code;
 extern size_t __secondary_boot_code_size;
 #ifdef CONFIG_MP
 int fsl_layerscape_wake_seconday_cores(void);
-int fsl_layerscape_wakeup_fixed_core(u32 coreid, u64 addr);
 #else
 static inline int fsl_layerscape_wake_seconday_cores(void) { return 0; }
 #endif
@@ -48,8 +48,6 @@ phys_addr_t determine_mp_bootpg(void);
 void secondary_boot_func(void);
 int is_core_online(u64 cpu_id);
 u32 cpu_pos_mask(void);
-int get_core_id(void);
-int is_core_valid(unsigned int core);
 #endif
 
 #endif /* _FSL_LAYERSCAPE_MP_H */
