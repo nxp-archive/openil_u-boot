6ba31f3b2af279188840e93cd69ebcafdff939fe
Laurentiu Tudor <laurentiu.tudor@nxp.com>
2019-03-19 17:05:29 +0200

    armv8: ls1088a: add icid setup for platform devices
    Add ICID setup for the platform devices contained on this chip: usb,
    sata, sdhc, sec. The ICID macros for SEC needed to be adapted because
    the format of the registers is different.
    
    Signed-off-by: Laurentiu Tudor <laurentiu.tudor@nxp.com>

--- board/freescale/ls1088a/ls1088a.c
+++ board/freescale/ls1088a/ls1088a.c
@@ -20,6 +20,7 @@
 #include <hwconfig.h>
 #include <asm/arch/fsl_serdes.h>
 #include <asm/arch/soc.h>
+#include <asm/arch-fsl-layerscape/fsl_icid.h>
 
 #include "../common/qixis.h"
 #include "ls1088a_qixis.h"
@@ -967,6 +968,9 @@ int ft_board_setup(void *blob, bd_t *bd)
 #ifdef CONFIG_FSL_MC_ENET
 	fdt_fixup_board_enet(blob);
 #endif
+
+	fdt_fixup_icid(blob);
+
 	if (is_pb_board())
 		fixup_ls1088ardb_pb_banner(blob);
 
