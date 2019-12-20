aace2594f147f4d48f75107bd8e9bba38aebfe9e
Jagdish Gediya <jagdish.gediya@nxp.com>
2019-12-20 11:48:12 +0530

    dm: arm64: ls1012a: add i2c DM support
    This supports i2c DM and enables CONFIG_DM_I2C
    for SoC LS1012A
    
    Signed-off-by: Biwen Li <biwen.li@nxp.com>

--- arch/arm/include/asm/gpio.h
+++ arch/arm/include/asm/gpio.h
@@ -3,6 +3,7 @@
 	!defined(CONFIG_ARCH_BCM63158) && !defined(CONFIG_ARCH_ROCKCHIP) && \
 	!defined(CONFIG_ARCH_LX2160A) && !defined(CONFIG_ARCH_LS1028A) && \
 	!defined(CONFIG_ARCH_LS2080A) && !defined(CONFIG_ARCH_LS1088A) && \
+	!defined(CONFIG_ARCH_LS1012A) && \
 	!defined(CONFIG_ARCH_ASPEED)
 #include <asm/arch/gpio.h>
 #endif
