1d19a85e6a2164f5c6d6e20787314f182521d7fb
Yi Zhao <yi.zhao@nxp.com>
2021-01-21 11:41:41 +0800

    baremetal: i.MX8MP-EVK support
    Enable GPIO and FEC-MXC ethernet.
    
    Signed-off-by: Yi Zhao <yi.zhao@nxp.com>
    Signed-off-by: Jerry Huang <jerry.huang@nxp.com>

---
+++ include/configs/imx8mp_evk_config.h
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2018-2019 NXP
+ *
+ */
+
+#define CONFIG_SLAVE_FIRST_CORE                 1
+
+/* set Master core size to 512M, slave core size to 256M, share mem to 256M */
+
+/* Number of cores in each cluster */
+#define CORE_NUM_PER_CLUSTER    4
+#define CONFIG_MAX_CPUS  4
